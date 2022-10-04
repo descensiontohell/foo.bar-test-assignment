4a13640452a744783deb9ce9b18be0719f86f69a
descensiontohell <isuhanen@gmail.com>
2022-10-04 16:02:22 +0300

    feat: add redirect to login on access denied

--- src/app/routers/errors.py
+++ src/app/routers/errors.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, redirect, render_template, flash
 
 errors_bp = Blueprint("errors", __name__)
 
@@ -8,14 +8,10 @@ def redirect_to_not_found(e):
 
 
 def redirect_to_access_denied(e):
-    return redirect("/denied")
+    flash("You should login to access the resource")
+    return redirect("/")
 
 
 @errors_bp.route("/notfound")
 def page_not_found():
     return render_template("notfound.html")
-
-
-@errors_bp.route("/denied")
-def access_denied():
-    return render_template("denied.html")
